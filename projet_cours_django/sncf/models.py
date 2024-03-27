c59e69f4d417af2adfe26b2ee3e2bbcf6e0ea2a8
alexis <alexis.gontier03@gmail.com>
2024-03-27 06:54:53 +0100

    initialisation du projet

---
+++ projet_cours_django/sncf/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# class Trains(models.Model):
+#     trainID = models.AutoField(primary_key=True)
+#     numeros = models.IntegerField()
+#     destination = models.CharField(max_length=30)
+#     heure_depart = models.TimeField()
+#     voie = models.CharField(max_length=30)
